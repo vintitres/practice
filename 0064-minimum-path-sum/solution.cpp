f471c375bafcc642fb6e6c9e8a3c9691b6a60c95
vintitres <vintitres@users.noreply.github.com>
2019-09-23 15:15:38 +0000

    [LeetCode Sync] Runtime - 12 ms (7.13%), Memory - 10.3 MB (100.00%)

--- 0064-minimum-path-sum/solution.cpp
+++ 0064-minimum-path-sum/solution.cpp
@@ -1,19 +1,21 @@
 class Solution {
 public:
     int minPathSum(vector<vector<int>>& grid) {
-        int m = grid.size();
-        int n = grid[0].size();
-        int row_below[n];
-        row_below[n - 1] = grid[m - 1][n - 1];
-        for (int j = n - 2; j >= 0; --j) {
-            row_below[j] = grid[m - 1][j] + row_below[j + 1];
-        }
-        for (int i = m - 2; i >= 0; --i) {
-            row_below[n - 1] = grid[i][n - 1] + row_below[n - 1];
-            for (int j = n - 2; j >= 0; --j) {
-                row_below[j] = grid[i][j] + min(row_below[j], row_below[j + 1]);
+        int row_size = grid[0].size();
+        vector<int> row = vector<int>(row_size);
+        for (int i = 0; i < grid.size(); ++i) {
+            for (int j = 0; j < row_size; ++j) {
+                int x;
+                if(j == 0) {
+                    x = row[0];
+                } else if (i == 0) {
+                    x = row[j - 1];
+                } else {
+                    x = min(row[j], row[j - 1]);
+                }
+                row[j] = grid[i][j] + x;
             }
         }
-        return row_below[0];
+        return row[row_size - 1];
     }
 };
