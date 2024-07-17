4687820833a7eda1e531d5d79090a577852e18a5
vintitres <vintitres@users.noreply.github.com>
2024-07-17 01:21:33 +0000

    [LeetCode Sync] Runtime - 4 ms (28.57%), Memory - 2.4 MB (71.43%)

--- leetcode/1207-delete-nodes-and-return-forest/solution.rs
+++ leetcode/1207-delete-nodes-and-return-forest/solution.rs
@@ -24,12 +24,12 @@ impl Solution {
         let mut forest = Vec::new();
         let mut root = root.borrow_mut();
         let del = to_delete.remove(&root.val);
-        let mut f = |child_ref: &mut Option<Rc<RefCell<TreeNode>>>, | {
+        let mut f = |child_ref: &mut Option<Rc<RefCell<TreeNode>>>| {
             if let Some(child) = &child_ref {
                 let (child_del, mut child_forest) = Self::_del_nodes(&child, to_delete);
                 forest.append(&mut child_forest);
                 if child_del {
-                    *child_ref = None
+                    *child_ref = None;
                 } else if del {
                     forest.push(Some(child.clone()));
                 }
