ff5ec57b7c947249411019bac2cf01fa149d363e
vintitres <vintitres@users.noreply.github.com>
2024-07-19 00:24:55 +0000

    [LeetCode Sync] Runtime - 2 ms (66.67%), Memory - 2.3 MB (66.67%)

--- leetcode/1496-lucky-numbers-in-a-matrix/solution.rs
+++ leetcode/1496-lucky-numbers-in-a-matrix/solution.rs
@@ -1,22 +1,17 @@
 impl Solution {
     pub fn lucky_numbers (matrix: Vec<Vec<i32>>) -> Vec<i32> {
-        let mut row_min : Vec<i32> = Vec::new();
-        let mut col_max = Vec::new();
-        for i in 0..matrix.len() {
-            row_min.push(*matrix[i].iter().min().unwrap());
-        }
-        for j in 0..matrix[0].len() {
-            col_max.push(
-                (0..matrix.len()).map(|i| matrix[i][j]).max().unwrap(),
-            );
-        }
+        let mut row_min : Vec<i32> = matrix.iter().map(|row|
+            *row.iter().min().unwrap()
+        ).collect();
+        let col_max : Vec<i32> = (0..matrix[0].len()).map(|j|
+            (0..matrix.len()).map(|i| matrix[i][j]).max().unwrap()
+        ).collect();
         let mut lucky = Vec::new();
         for i in 0..matrix.len() {
             for j in 0..matrix[i].len() {
                 if matrix[i][j] == row_min[i] && matrix[i][j] == col_max[j] {
                     lucky.push(matrix[i][j]);
                 }
-
             }
         }
         lucky
