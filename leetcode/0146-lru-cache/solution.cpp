bf176c9fde06417dd026d04e30f75bef088eb485
vintitres <vintitres@users.noreply.github.com>
2024-08-09 02:08:27 +0000

    [LeetCode Sync] Runtime - 377 ms (33.33%), Memory - 179.2 MB (43.16%)

--- leetcode/0146-lru-cache/solution.cpp
+++ leetcode/0146-lru-cache/solution.cpp
@@ -1,22 +1,25 @@
 class LRUCache {
     int capacity;
-    unordered_map<int, int> cache;
-    list<int> access_queue;
-    unordered_map<int, list<int>::iterator> queue_pos;
+    list<pair<int,int>> access_queue;
+    unordered_map<int, list<pair<int, int>>::iterator> queue_pos;
 
-    void add_access(int key) {
-        auto it = queue_pos.find(key);
+    void add_access(unordered_map<int,list<pair<int,int>>::iterator>::iterator it, int key, int val) {
         if (it == queue_pos.end()) {
-            access_queue.push_front(key);
-            queue_pos[key] = access_queue.begin(); 
+            if (queue_pos.size() == capacity) {
+                auto back = access_queue.back();
+                access_queue.pop_back();
+                queue_pos.erase(back.first);
+            }
+            access_queue.push_front({key, val});
+            queue_pos[key] = access_queue.begin();
         } else if (it->second == access_queue.begin()) {
+            it->second->second = val;
             return;
         } else if (it != queue_pos.end()) {
             access_queue.erase(it->second);
-            access_queue.push_front(key);
+            access_queue.push_front({key, val});
             it->second = access_queue.begin();
         }
-             
     }
 
 public:
@@ -24,28 +27,19 @@ public:
     }
     
     int get(int key) {
-        auto it = cache.find(key);
-        if (it == cache.end()) {
+        auto it = queue_pos.find(key);
+        if (it == queue_pos.end()) {
             return -1;
         }
-        add_access(key);
-        return it->second;
+        assert(it->second->first == key);
+        int val = it->second->second;
+        add_access(it, key, val);
+        return val;
     }
     
     void put(int key, int value) {
-        auto it = cache.find(key);
-        if (it == cache.end()) {
-            if (cache.size() == capacity) {
-                int x = access_queue.back();
-                access_queue.pop_back();
-                queue_pos.erase(x);
-                cache.erase(x);
-            }
-            cache[key] = value;
-        } else {
-            it->second = value;
-        }
-        add_access(key);
+        auto it = queue_pos.find(key);
+        add_access(it, key, value);
     }
 };
 
