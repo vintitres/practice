e66dbdbc3606b7524525e7e4f4b4debe9ce03d19
vintitres <vintitres@users.noreply.github.com>
2024-08-10 01:09:50 +0000

    [LeetCode Sync] Runtime - 68 ms (6.78%), Memory - 50.4 MB (5.15%)

--- leetcode/0999-regions-cut-by-slashes/solution.cpp
+++ leetcode/0999-regions-cut-by-slashes/solution.cpp
@@ -1,5 +1,5 @@
 const char SIDES[] = {'l', 'r', 'u', 'd'};
-typedef set<tuple<int, int, char>> VisSet;
+typedef set<tuple<int, int, char>> VisSet;  // could be 3 dim array of bools (grid.size() x grid[0].size() x 4)
 
 class Solution {
     void fill_area(int x, int y, char side, vector<string> const& grid,
