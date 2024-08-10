77c80350ca2dba76662e132c306222ae08ce73c5
vintitres <vintitres@users.noreply.github.com>
2024-08-10 01:27:03 +0000

    [LeetCode Sync] Runtime - 57 ms (7.86%), Memory - 26.2 MB (7.32%)

--- leetcode/0999-regions-cut-by-slashes/solution.cpp
+++ leetcode/0999-regions-cut-by-slashes/solution.cpp
@@ -4,42 +4,57 @@ typedef set<tuple<int, int, char>> VisSet;  // could be 3 dim array of bools (gr
 class Solution {
     void fill_area(int x, int y, char side, vector<string> const& grid,
                    VisSet& vis) {
-        if (x < 0 || x >= grid.size() || y < 0 || y >= grid[0].size() ||
-            vis.contains({x, y, side})) {
-            return;
-        }
-        // cout << x << " " << y << " " << side << endl;
-        if (grid[x][y] == ' ') {
-            for (char side : SIDES) {
-                vis.insert({x, y, side});
+        queue<tuple<int, int, char>> q;
+        q.push({x,y,side});
+        auto qadd = [&](tuple<int, int, char> el) {
+            auto [x, y, s] = el;
+            if (x < 0 || x >= grid.size() || y < 0 || y >= grid[0].size()) {
+                return;
             }
-            fill_area(x + 1, y, 'u', grid, vis);
-            fill_area(x - 1, y, 'd', grid, vis);
-            fill_area(x, y + 1, 'l', grid, vis);
-            fill_area(x, y - 1, 'r', grid, vis);
-        } else if (grid[x][y] == '/') {
-            if (side == 'l' || side == 'u') {
-                vis.insert({x, y, 'l'});
-                vis.insert({x, y, 'u'});
-                fill_area(x - 1, y, 'd', grid, vis);
-                fill_area(x, y - 1, 'r', grid, vis);
-            } else {
-                vis.insert({x, y, 'r'});
-                vis.insert({x, y, 'd'});
-                fill_area(x + 1, y, 'u', grid, vis);
-                fill_area(x, y + 1, 'l', grid, vis);
+            if (!vis.contains(el)) {
+                q.push(el);
             }
-        } else { // "\"
-            if (side == 'l' || side == 'd') {
-                vis.insert({x, y, 'l'});
-                vis.insert({x, y, 'd'});
-                fill_area(x + 1, y, 'u', grid, vis);
-                fill_area(x, y - 1, 'r', grid, vis);
-            } else {
-                vis.insert({x, y, 'r'});
-                vis.insert({x, y, 'u'});
-                fill_area(x - 1, y, 'd', grid, vis);
-                fill_area(x, y + 1, 'l', grid, vis);
+        };
+            
+        while (!q.empty()) {
+            auto xys = q.front();
+            q.pop();
+            if (vis.contains(xys)) {
+                continue;
+            }
+            auto [x, y, side] = xys;
+            if (grid[x][y] == ' ') {
+                for (char side : SIDES) {
+                    vis.insert({x, y, side});
+                }
+                qadd({x + 1, y, 'u'});
+                qadd({x - 1, y, 'd'});
+                qadd({x, y + 1, 'l'});
+                qadd({x, y - 1, 'r'});
+            } else if (grid[x][y] == '/') {
+                if (side == 'l' || side == 'u') {
+                    vis.insert({x, y, 'l'});
+                    vis.insert({x, y, 'u'});
+                    qadd({x - 1, y, 'd'});
+                    qadd({x, y - 1, 'r'});
+                } else {
+                    vis.insert({x, y, 'r'});
+                    vis.insert({x, y, 'd'});
+                    qadd({x + 1, y, 'u'});
+                    qadd({x, y + 1, 'l'});
+                }
+            } else { // "\"
+                if (side == 'l' || side == 'd') {
+                    vis.insert({x, y, 'l'});
+                    vis.insert({x, y, 'd'});
+                    qadd({x + 1, y, 'u'});
+                    qadd({x, y - 1, 'r'});
+                } else {
+                    vis.insert({x, y, 'r'});
+                    vis.insert({x, y, 'u'});
+                    qadd({x, y + 1, 'l'});
+                    qadd({x - 1, y, 'd'});
+                }
             }
         }
     }
