a13cb7b254c7635de0f915ef983efc414ec35ce5
vintitres <vintitres@users.noreply.github.com>
2024-09-12 01:05:22 +0000

    [LeetCode Sync] Runtime - 220 ms (47.25%), Memory - 18.5 MB (79.72%)

--- leetcode/1786-count-the-number-of-consistent-strings/solution.py
+++ leetcode/1786-count-the-number-of-consistent-strings/solution.py
@@ -1,15 +1,6 @@
 class Solution:
     def countConsistentStrings(self, allowed: str, words: List[str]) -> int:
         allowed = set([c for c in allowed])
-        consistent = 0
-        for word in words:
-            ok = True
-            for c in word:
-                if c not in allowed:
-                    ok = False
-                    break
-            if ok:
-                consistent += 1
-        return consistent
+        return sum(all(c in allowed for c in word) for word in words)
             
         
