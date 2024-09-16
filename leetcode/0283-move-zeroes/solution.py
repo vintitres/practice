11b488c3c3ebee856b6d272961133c0cb5411066
vintitres <vintitres@users.noreply.github.com>
2024-09-16 15:28:15 +0000

    [LeetCode Sync] Runtime - 129 ms (41.68%), Memory - 18.1 MB (61.41%)

--- leetcode/0283-move-zeroes/solution.py
+++ leetcode/0283-move-zeroes/solution.py
@@ -4,7 +4,6 @@ class Solution:
         Do not return anything, modify nums in-place instead.
         """
         zeroes_start = 0
-        zeroes_end = 0
         for i in range(len(nums)):
             if nums[i] != 0:
                 if zeroes_start < i:
