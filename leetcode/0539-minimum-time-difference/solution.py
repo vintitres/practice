082f679db8778a5fdd1cc2662d4bb1ab7bf1e5f2
vintitres <vintitres@users.noreply.github.com>
2024-09-16 14:27:22 +0000

    [LeetCode Sync] Runtime - 63 ms (86.08%), Memory - 19.6 MB (28.85%)

--- leetcode/0539-minimum-time-difference/solution.py
+++ leetcode/0539-minimum-time-difference/solution.py
@@ -2,17 +2,13 @@ class Solution:
     def minutes(timePoint: str) -> int:
         hours = int(timePoint[:2])
         minutes = int(timePoint[3:])
-        # print(hours, minutes)
         return hours * 60 + minutes
 
     def findMinDifference(self, timePoints: List[str]) -> int:
         timePoints = [Solution.minutes(tp) for tp in sorted(timePoints)]
         lastTimePoint = timePoints[0]
         minDiff = timePoints[0] + 60 * 24 - timePoints[-1]
-        # print(Solution.minutes(timePoints[0]))
         for timePoint in timePoints[1:]:
-            # print(Solution.minutes(timePoint))
-            # print(minDiff, "D")
             minDiff = min(minDiff, timePoint - lastTimePoint)
             if minDiff == 0:
                 return 0
