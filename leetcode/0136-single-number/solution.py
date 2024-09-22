102aee1c848b4f3e8416fd61e17c86beb19e0b8f
vintitres <vintitres@users.noreply.github.com>
2024-09-22 19:53:09 +0000

    [LeetCode Sync] Runtime - 116 ms (31.17%), Memory - 19.5 MB (5.74%)

--- leetcode/0136-single-number/solution.py
+++ leetcode/0136-single-number/solution.py
@@ -1,7 +1,4 @@
+import itertools
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
-        xorall = 0
-        for num in nums:
-            xorall ^= num
-        return xorall
-        
+        return list(itertools.accumulate(nums, xor))[-1]
