f1be9e7564d25d87712a0e21ddc5123608bbea3d
vintitres <vintitres@users.noreply.github.com>
2024-09-22 01:22:52 +0000

    [LeetCode Sync] Runtime - 292 ms (11.29%), Memory - 26.5 MB (7.78%)

--- leetcode/0238-product-of-array-except-self/solution.py
+++ leetcode/0238-product-of-array-except-self/solution.py
@@ -1,16 +1,15 @@
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
-        productR = []
+        productR = [None] * len(nums)
         product = 1
-        for n in reversed(nums):
+        for i, n in enumerate(reversed(nums)):
             product *= n
-            productR.append(product)
-        productR.reverse()
+            productR[len(nums) - 1 - i] = product
         def get(product, index):
             return product[index] if index >= 0 and index < len(product) else 1
         product = 1
-        ret = []
+        ret = [None] * len(nums)
         for i, num in enumerate(nums):
-            ret.append(product * get(productR, i + 1))
+            ret[i] = product * get(productR, i + 1)
             product *= num
         return ret
