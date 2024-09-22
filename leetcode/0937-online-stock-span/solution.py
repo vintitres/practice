1ae7b05f6a8ec520e97fd1c358904bc58d20f811
vintitres <vintitres@users.noreply.github.com>
2024-09-22 19:20:36 +0000

    [LeetCode Sync] Runtime - 279 ms (63.50%), Memory - 22.4 MB (6.98%)

--- leetcode/0937-online-stock-span/solution.py
+++ leetcode/0937-online-stock-span/solution.py
@@ -1,17 +1,22 @@
 class StockSpanner:
     price_history: List[int]
+    skip: List[int]
 
     def __init__(self):
         self.price_history = []
+        self.skip = []
         
 
     def next(self, price: int) -> int:
         length = 1
-        for ph in reversed(self.price_history):
-            if ph <= price:
-                length += 1
+        i = len(self.price_history) - 1
+        while i >= 0:
+            if self.price_history[i] <= price:
+                length += self.skip[i]
+                i -= self.skip[i]
             else:
                 break
+        self.skip.append(length)
         self.price_history.append(price)
         return length
         
