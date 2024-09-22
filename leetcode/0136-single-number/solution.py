2596f7079d9d708998925ff8b593cf3d4e1991a4
vintitres <vintitres@users.noreply.github.com>
2024-09-22 19:57:51 +0000

    [LeetCode Sync] Runtime - 110 ms (58.90%), Memory - 19.3 MB (9.64%)

--- leetcode/0136-single-number/solution.py
+++ leetcode/0136-single-number/solution.py
@@ -1,5 +1,4 @@
-import itertools
+import functools
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
-        *_, last = itertools.accumulate(nums, lambda a,b: a^b)
-        return last
+        return functools.reduce(xor, nums)
