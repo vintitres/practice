5cf93f430253759d000ae482f553b844435a9154
vintitres <vintitres@users.noreply.github.com>
2024-09-22 18:27:37 +0000

    [LeetCode Sync] Runtime - 176 ms (77.46%), Memory - 32.2 MB (5.06%)

---
+++ leetcode/1474-longest-zigzag-path-in-a-binary-tree/solution.py
@@ -0,0 +1,29 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def longestZigZag_(self, root: Optional[TreeNode]) -> (int, int, int):
+        if root is None:
+            return (0, 0, 0)
+        left = 1
+        right = 1
+        mx = 1
+        if root.left is not None:
+            ll, lr, lmx = self.longestZigZag_(root.left)
+            left += lr
+            mx = max(mx, ll, left, lmx)
+        if root.right is not None:
+            rl, rr, rmx = self.longestZigZag_(root.right)
+            right += rl
+            mx = max(mx, rr, right, rmx)
+        return left, right, mx
+            
+            
+            
+
+    def longestZigZag(self, root: Optional[TreeNode]) -> int:
+        _, _, mx = self.longestZigZag_(root)
+        return mx - 1
