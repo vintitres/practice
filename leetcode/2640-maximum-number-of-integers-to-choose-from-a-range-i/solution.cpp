615a1c897f8c6202587f98f434d9ddcd25d051cd
vintitres <vintitres@users.noreply.github.com>
2024-12-06 04:57:24 +0000

    [LeetCode Sync] Runtime - 566 ms (7.10%), Memory - 117.6 MB (88.58%)

---
+++ leetcode/2640-maximum-number-of-integers-to-choose-from-a-range-i/solution.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int maxCount(vector<int>& banned, int n, int maxSum) {
+        sort(banned.begin(), banned.end());
+        int sum = 0;
+        auto banned_it = banned.begin();
+        int count = 0;
+        for (int i = 1; i <= n; ++i) {
+            if (banned_it != banned.end() && *banned_it == i) {
+                while (banned_it != banned.end() && *banned_it == i) {
+                    ++banned_it;
+                }
+                continue;
+            }
+            sum += i;
+            if (sum > maxSum) {
+                cout << "!" << i << endl;
+                break;
+            }
+            cout << i <<endl;
+            ++count;
+        }
+        cout<< "=" << sum << endl;
+        return count;
+    }
+};
