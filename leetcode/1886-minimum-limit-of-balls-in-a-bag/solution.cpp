a79af068e3e6c9957c40dd2a8f287c2ee4471f5f
vintitres <vintitres@users.noreply.github.com>
2024-12-07 21:41:28 +0000

    [LeetCode Sync] Runtime - 26 ms (85.76%), Memory - 59.6 MB (96.15%)

---
+++ leetcode/1886-minimum-limit-of-balls-in-a-bag/solution.cpp
@@ -0,0 +1,28 @@
+class Solution {
+    bool canDo(vector<int>& nums, int maxOperations, int penalty) {
+        int op = 0;
+        for (int num : nums) {
+            op += num / penalty;
+            if (num % penalty == 0) {
+                op -= 1;
+            }
+            if (op > maxOperations) {
+                return false;
+            }
+        }
+        return true;
+    }
+public:
+    int minimumSize(vector<int>& nums, int maxOperations) {
+        int b = 1, e = 1000000001;
+        while (b < e) {
+            int m = (e + b) / 2;
+            if (canDo(nums, maxOperations, m)) {
+                e = m;
+            } else {
+                b = m + 1;
+            }
+        }
+        return e;
+    }
+};
