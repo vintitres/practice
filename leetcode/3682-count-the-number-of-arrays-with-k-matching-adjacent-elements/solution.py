0e586e81c674ded113cd19aac139b1c5c2903860
vintitres <vintitres@users.noreply.github.com>
2025-06-17 21:22:29 +0000

    [LeetCode Sync] Runtime - 1760 ms (76.39%), Memory - 17.9 MB (95.83%)

--- leetcode/3682-count-the-number-of-arrays-with-k-matching-adjacent-elements/solution.py
+++ leetcode/3682-count-the-number-of-arrays-with-k-matching-adjacent-elements/solution.py
@@ -1,16 +1,4 @@
 class Solution:
-    def modpow(base, power, mod):
-        res = 1 
-        subpow = base
-        while power > 0:
-            if int(power % 2) == 1:
-                res *= subpow
-                res = int(res % mod)
-            power /= 2
-            subpow *= subpow
-            subpow = int(subpow % mod)
-        return res
-
     def modexcl(n, mod):
         res = 1
         for i in range(1, n + 1):
@@ -19,10 +7,9 @@ class Solution:
         return res
     
     def modinv(n, mod):
-        return Solution.modpow(n, mod - 2, mod)
+        return pow(n, mod - 2, mod)
     
     def modbicoef(n, k, mod):
-        print(n, k)
         num = Solution.modexcl(n, mod)
         denom = int((Solution.modexcl(k, mod) * Solution.modexcl(n - k, mod)) % mod)
         return int((num * Solution.modinv(denom, mod)) % mod)
@@ -37,7 +24,7 @@ class Solution:
         # how many combinations of k elements form n - 1 elements
         divisions = Solution.modbicoef(n - 1, k, mod)
         # how many ways can we color k + 1 sections with m colors, but can't have 2 consecutive same colors
-        colorings = m * Solution.modpow(m - 1, k, mod)
+        colorings = m * pow(m - 1, k, mod)
         print(divisions, colorings)
         return int((divisions * colorings) % mod)
 
