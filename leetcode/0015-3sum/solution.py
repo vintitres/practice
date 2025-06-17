57aa252123bcce87e8d41f9631b9b6d15af79217
vintitres <vintitres@users.noreply.github.com>
2025-06-17 17:05:41 +0000

    [LeetCode Sync] Runtime - 811 ms (25.06%), Memory - 20.7 MB (37.01%)

--- leetcode/0015-3sum/solution.py
+++ leetcode/0015-3sum/solution.py
@@ -2,13 +2,10 @@ class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
         nums = sorted(nums)  # create a sorted copy
         triplets = []
-        last = nums[0] + 1
         for i in range(0, len(nums)):
             n = nums[i]
-            if last == n:
+            if i > 0 and nums[i - 1] == n:
                 continue
-            else:
-                last = n
             j = i + 1
             k = len(nums) - 1
             while j < k:
@@ -17,10 +14,14 @@ class Solution:
                 s = n + m + o
                 if s > 0:
                     k -= 1
+                    while j < k and nums[k] == nums[k + 1]: 
+                        k -= 1
                 else:
                     if s == 0 and (not triplets or triplets[-1][1] != m or triplets[-1][2] != o):
                         triplets.append([n, m, o])
                     j += 1
+                    while j < k and nums[j] == nums[j - 1]:
+                        j += 1
         return triplets
 
         
