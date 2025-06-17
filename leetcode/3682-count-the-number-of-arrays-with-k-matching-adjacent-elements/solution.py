5a56da7bf488a198f537124e15df186dd1ee3dba
vintitres <vintitres@users.noreply.github.com>
2025-06-17 21:27:16 +0000

    [LeetCode Sync] Runtime - 1447 ms (76.39%), Memory - 17.9 MB (95.83%)

--- leetcode/3682-count-the-number-of-arrays-with-k-matching-adjacent-elements/solution.py
+++ leetcode/3682-count-the-number-of-arrays-with-k-matching-adjacent-elements/solution.py
@@ -1,18 +1,19 @@
 class Solution:
-    def modexcl(n, mod):
+    MOD = 10**9 + 7
+    def modexcl(n):
         res = 1
         for i in range(1, n + 1):
             res *= i
-            res = int(res % mod)
+            res = res % Solution.MOD
         return res
     
-    def modinv(n, mod):
-        return pow(n, mod - 2, mod)
+    def modinv(n):
+        return pow(n, Solution.MOD - 2, Solution.MOD)
     
-    def modbicoef(n, k, mod):
-        num = Solution.modexcl(n, mod)
-        denom = int((Solution.modexcl(k, mod) * Solution.modexcl(n - k, mod)) % mod)
-        return int((num * Solution.modinv(denom, mod)) % mod)
+    def modbicoef(n, k):
+        num = Solution.modexcl(n)
+        denom = int((Solution.modexcl(k) * Solution.modexcl(n - k)) % Solution.MOD)
+        return int((num * Solution.modinv(denom)) % Solution.MOD)
 
     def countGoodArrays(self, n: int, m: int, k: int) -> int:
         mod = 10**9 + 7
@@ -22,10 +23,10 @@ class Solution:
         # how many combinations: x..x y..y x..x y..y x..x y..y of k + 1 sections in n elems
         # how can i pick k split points in n elem array
         # how many combinations of k elements form n - 1 elements
-        divisions = Solution.modbicoef(n - 1, k, mod)
+        divisions = Solution.modbicoef(n - 1, k)
         # how many ways can we color k + 1 sections with m colors, but can't have 2 consecutive same colors
-        colorings = m * pow(m - 1, k, mod)
-        return int((divisions * colorings) % mod)
+        colorings = m * pow(m - 1, k, Solution.MOD)
+        return int((divisions * colorings) % Solution.MOD)
 
 
 
