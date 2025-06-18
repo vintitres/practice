bfabb3b4c92218c4d2eda216d5bcaec93872e046
vintitres <vintitres@users.noreply.github.com>
2025-06-18 20:50:29 +0000

    [LeetCode Sync] Runtime - 83 ms (10.11%), Memory - 20.3 MB (7.84%)

---
+++ leetcode/0137-single-number-ii/solution.py
@@ -0,0 +1,24 @@
+class Solution:
+    BITS: int = 32
+    def singleNumber(self, nums: List[int]) -> int:
+        bit_count = [0] * Solution.BITS
+        nums = [n + 2 ** (Solution.BITS - 1) for n in nums]
+        for n in nums:
+            i = 0
+            while n > 0:
+                bit_count[i] += n % 2
+                i += 1
+                n = int(n / 2)
+        x = 0
+        pow2 = 1
+        print(bit_count)
+        for i in range(Solution.BITS):
+            if bit_count[i] % 3 != 0:
+                x += pow2
+            pow2 *= 2
+        return x - 2 ** (Solution.BITS - 1)
+
+
+
+        
+        
