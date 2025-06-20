2ecdab2143cfa13be5a9aac53074dc7b1bf3ac89
vintitres <vintitres@users.noreply.github.com>
2025-06-20 19:55:04 +0000

    [LeetCode Sync] Runtime - 411 ms (24.21%), Memory - 18.7 MB (93.24%)

--- leetcode/0127-word-ladder/solution.py
+++ leetcode/0127-word-ladder/solution.py
@@ -8,16 +8,16 @@ class Solution:
         wordSet = set(wordList)
         if endWord not in wordSet:
             return 0
-        seen = set([beginWord])
         q = deque([(beginWord, 1)])
+        if beginWord in wordSet:
+            wordSet.remove(beginWord)
         while q:
             (word, steps) = q.popleft()
-            print('w', word)
             for nextWord in self.nextWords(word):
                 if nextWord == endWord:
                     return steps + 1
-                if nextWord in wordSet and nextWord not in seen:
-                    seen.add(nextWord)
+                if nextWord in wordSet:
+                    wordSet.remove(nextWord)
                     q.append((nextWord, steps + 1))
         return 0
 
