ee9de4a5c801ed9afe29613cf579a837bb02b07d
vintitres <vintitres@users.noreply.github.com>
2025-06-20 20:33:09 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 18.3 MB (20.17%)

--- leetcode/0274-h-index/solution.py
+++ leetcode/0274-h-index/solution.py
@@ -1,10 +1,11 @@
 class Solution:
     def hIndex(self, citations: List[int]) -> int:
         begin = 0
+        citations.sort()
         end = len(citations)
         while begin < end:
             mid = int((begin + end + 1) / 2)
-            if len(list(filter(lambda x: x >= mid, citations))) < mid:
+            if len(citations) - bisect_left(citations, mid) < mid:
                 end = mid - 1
             else:
                 begin = mid
