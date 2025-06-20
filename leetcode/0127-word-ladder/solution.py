fbfae8d8b509b6a5862f5bc08c54c17748ffd6d2
vintitres <vintitres@users.noreply.github.com>
2025-06-20 19:56:46 +0000

    [LeetCode Sync] Runtime - 309 ms (44.34%), Memory - 18.7 MB (85.33%)

--- leetcode/0127-word-ladder/solution.py
+++ leetcode/0127-word-ladder/solution.py
@@ -1,9 +1,4 @@
 class Solution:
-    def nextWords(self, word: str):
-        for i in range(len(word)):
-            for c in string.ascii_lowercase:
-                yield word[:i] + c + word[i + 1:]
-
     def ladderLength(self, beginWord: str, endWord: str, wordList: List[str]) -> int:
         wordSet = set(wordList)
         if endWord not in wordSet:
@@ -13,11 +8,13 @@ class Solution:
             wordSet.remove(beginWord)
         while q:
             (word, steps) = q.popleft()
-            for nextWord in self.nextWords(word):
-                if nextWord == endWord:
-                    return steps + 1
-                if nextWord in wordSet:
-                    wordSet.remove(nextWord)
-                    q.append((nextWord, steps + 1))
+            for i in range(len(word)):
+                for c in string.ascii_lowercase:
+                    nextWord = word[:i] + c + word[i + 1:]
+                    if nextWord == endWord:
+                        return steps + 1
+                    if nextWord in wordSet:
+                        wordSet.remove(nextWord)
+                        q.append((nextWord, steps + 1))
         return 0
 
