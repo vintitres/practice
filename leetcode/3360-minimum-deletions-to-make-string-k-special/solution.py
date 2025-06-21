ec9da073265933e91330777643a9e4a88edb812d
vintitres <vintitres@users.noreply.github.com>
2025-06-21 18:58:13 +0000

    [LeetCode Sync] Runtime - 59 ms (68.29%), Memory - 18.4 MB (20.12%)

---
+++ leetcode/3360-minimum-deletions-to-make-string-k-special/solution.py
@@ -0,0 +1,19 @@
+from collections import Counter
+
+class Solution:
+    def minimumDeletions(self, word: str, k: int) -> int:
+        counts = sorted(Counter(word).values())
+        begin_sum = 0
+        mi = len(word)
+        print(counts)
+        for begin in range(len(counts)):
+            end_sum = 0
+            for end in reversed(range(begin, len(counts))):
+                if counts[end] - counts[begin] <= k:
+                    print(begin, end, begin_sum, end_sum)
+                    mi = min(mi, begin_sum + end_sum)
+                    break
+                end_sum += counts[end] - (counts[begin] + k)
+            begin_sum += counts[begin]
+        return mi
+        
