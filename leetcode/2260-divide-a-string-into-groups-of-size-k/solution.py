5d8b1f1e339e3f32f7987a1dd28fa6e27e5765c8
vintitres <vintitres@users.noreply.github.com>
2025-06-23 02:11:34 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 18.1 MB (18.56%)

--- leetcode/2260-divide-a-string-into-groups-of-size-k/solution.py
+++ leetcode/2260-divide-a-string-into-groups-of-size-k/solution.py
@@ -3,7 +3,6 @@ class Solution:
         ret = []
         for i in range(math.ceil(len(s) / k)):
             ret.append(s[i * k:i * k + k])
-        if len(ret[-1]) < k:
-            ret[-1] = ret[-1] + fill * (k - len(ret[-1]))
+        ret[-1] = ret[-1] + fill * (k - len(ret[-1]))
         return ret
         
