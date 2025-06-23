f09fd439d1e26232a161067574dbc87dca6f2291
vintitres <vintitres@users.noreply.github.com>
2025-06-23 17:08:38 +0000

    [LeetCode Sync] Runtime - 1189 ms (64.18%), Memory - 18.3 MB (10.06%)

---
+++ leetcode/0300-longest-increasing-subsequence/solution.py
@@ -0,0 +1,9 @@
+class Solution:
+    def lengthOfLIS(self, nums: List[int]) -> int:
+        longest = [1] * len(nums)
+        for i, num in enumerate(nums):
+            longest[i] = max(longest[j] if num > nums[j] else 0 for j in range(i)or [0]) + 1
+        print(longest)
+        return max(longest)
+
+        
