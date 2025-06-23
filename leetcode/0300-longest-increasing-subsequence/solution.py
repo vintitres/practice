09acf5b88c872645340e7884ac598b0bf166fe08
vintitres <vintitres@users.noreply.github.com>
2025-06-23 17:08:47 +0000

    [LeetCode Sync] Runtime - 1201 ms (64.13%), Memory - 18 MB (54.16%)

--- leetcode/0300-longest-increasing-subsequence/solution.py
+++ leetcode/0300-longest-increasing-subsequence/solution.py
@@ -3,7 +3,6 @@ class Solution:
         longest = [1] * len(nums)
         for i, num in enumerate(nums):
             longest[i] = max(longest[j] if num > nums[j] else 0 for j in range(i)or [0]) + 1
-        print(longest)
         return max(longest)
 
         
