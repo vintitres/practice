06c72b4199e04101ca37105242c16503fe57717d
vintitres <vintitres@users.noreply.github.com>
2025-06-24 19:00:56 +0000

    [LeetCode Sync] Runtime - 3 ms (37.02%), Memory - 21.2 MB (26.84%)

--- leetcode/0230-kth-smallest-element-in-a-bst/solution.py
+++ leetcode/0230-kth-smallest-element-in-a-bst/solution.py
@@ -12,12 +12,31 @@ class Solution:
 
         
     def kthSmallest(self, root: Optional[TreeNode], k: int) -> int:
+        """
         size_left = Solution.size(root.left)
         if size_left >= k:
             return self.kthSmallest(root.left, k)
         if size_left == k - 1:
             return root.val
         return self.kthSmallest(root.right, k - size_left - 1)
+        """
+        cnt = 0
+        queue = deque([(root, False)])
+        while queue:
+            node, processed = queue.popleft()
+            print(node.val, processed)
+            if not processed:
+                if node.right:
+                    queue.appendleft((node.right, False))
+                queue.appendleft((node, True))
+                if node.left:
+                    queue.appendleft((node.left, False))
+            else:
+                cnt += 1
+                if cnt == k:
+                    return node.val
+
+
 
 
         
