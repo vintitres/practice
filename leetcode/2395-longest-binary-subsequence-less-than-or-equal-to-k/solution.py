bdab6833b6980a051734a81113ce3fc16821e7f0
vintitres <vintitres@users.noreply.github.com>
2025-06-26 03:48:59 +0000

    [LeetCode Sync] Runtime - 11 ms (30.89%), Memory - 17.9 MB (47.97%)

--- leetcode/2395-longest-binary-subsequence-less-than-or-equal-to-k/solution.py
+++ leetcode/2395-longest-binary-subsequence-less-than-or-equal-to-k/solution.py
@@ -4,10 +4,6 @@ class Solution:
         i = 0
         deleted = 0
         bit_n = 0
-        print(s)
-        print(bin(x))
-        print(bin(bit_n))
-        print("w")
         had_one = False
         while x > k:
             while i < len(s) and s[i] == '0':
@@ -19,11 +15,6 @@ class Solution:
             if not had_one:
                 had_one = True
                 bit_n = 1 << (len(s) - i - 1)
-                """
-            print(bin(bit_n))
-            print(bin(x))
-            print(i)
-            """
             x -= bit_n
             i += 1
             bit_n //= 2
