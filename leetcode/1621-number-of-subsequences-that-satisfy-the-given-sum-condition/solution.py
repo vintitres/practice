0f124d4d770750fe6ac09a1d137f8d18b62a8e05
vintitres <vintitres@users.noreply.github.com>
2025-06-29 03:22:39 +0000

    [LeetCode Sync] Runtime - 270 ms (40.33%), Memory - 28.4 MB (7.74%)

---
+++ leetcode/1621-number-of-subsequences-that-satisfy-the-given-sum-condition/solution.py
@@ -0,0 +1,28 @@
+class Solution:
+    def excl_sum_mod(n: int, mod: int) -> int:
+        x = 1
+        s = 0
+        for i in range(1, n + 1):
+            x *= i
+            x %= mod
+            s += x
+            s %= mod
+        return s
+
+    def numSubseq(self, nums: List[int], target: int) -> int:
+        MOD = 10 ** 9 + 7
+        nums = sorted(nums)
+        i = 0
+        j = len(nums) - 1
+        count = 0
+        while i <= j:
+            while j > i and nums[i] + nums[j] > target:
+                j -= 1
+            if nums[i] + nums[j] > target:
+                break
+            count += pow(2, j - i, MOD) 
+            count %= MOD
+            print(i, j, count)
+            i += 1
+        return count
+        
