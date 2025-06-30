4b7e07d606afa229cf0e19ab2dcc51822ae32db3
vintitres <vintitres@users.noreply.github.com>
2025-06-30 22:49:57 +0000

    [LeetCode Sync] Runtime - 1214 ms (69.64%), Memory - 18.1 MB (85.03%)

--- leetcode/0718-maximum-length-of-repeated-subarray/solution.py
+++ leetcode/0718-maximum-length-of-repeated-subarray/solution.py
@@ -1,13 +1,14 @@
 class Solution:
     def findLength(self, nums1: List[int], nums2: List[int]) -> int:
         mx = 0
-        last_lengths = [0 for _ in range(len(nums2))]
+        last_lengths = [0] * len(nums2)
         for i in reversed(range(len(nums1))):
-            lengths = [0 for _ in range(len(nums2))]
+            lengths = [0] * len(nums2) 
             for j in reversed(range(len(nums2))):
                 if nums1[i] == nums2[j]:
-                    lengths[j] = 1 + (last_lengths[j + 1] if j + 1 < len(nums2) else 0)
-                    mx = max(mx, lengths[j])
+                    l = 1 + (last_lengths[j + 1] if j + 1 < len(nums2) else 0)
+                    mx = max(mx, l)
+                    lengths[j] = l
             last_lengths = lengths
         return mx
 
