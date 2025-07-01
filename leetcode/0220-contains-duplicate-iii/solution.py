8f6c54d6890e608989606be085e50d85ce99f318
vintitres <vintitres@users.noreply.github.com>
2025-07-01 02:55:49 +0000

    [LeetCode Sync] Runtime - 159 ms (55.81%), Memory - 41.1 MB (10.13%)

--- leetcode/0220-contains-duplicate-iii/solution.py
+++ leetcode/0220-contains-duplicate-iii/solution.py
@@ -1,26 +1,22 @@
 class Solution:
     def containsNearbyAlmostDuplicate(self, nums: List[int], indexDiff: int, valueDiff: int) -> bool:
-        buckets = defaultdict(Counter)
+        buckets = {}
         min_num = min(nums)
         nums = [num - min_num for num in nums]
         for i, num in enumerate(nums):
             num_bucket = num // (valueDiff + 1)
-            if num_bucket in buckets and len(buckets[num_bucket]):
+            if num_bucket in buckets:
                 return True
             if num_bucket + 1 in buckets:
-                for v in buckets[num_bucket + 1].keys():
-                    if abs(num - v) <= valueDiff:
-                        return True
+                if abs(num - buckets[num_bucket + 1]) <= valueDiff:
+                    return True
             if num_bucket - 1 in buckets:
-                for v in buckets[num_bucket - 1].keys():
-                    if abs(num - v) <= valueDiff:
-                        return True
-            buckets[num_bucket].update([num])
+                if abs(num - buckets[num_bucket - 1]) <= valueDiff:
+                    return True
+            buckets[num_bucket] = num
             if i >= indexDiff:
                 num_to_remove = nums[i - indexDiff]
-                buckets[num_to_remove // (valueDiff + 1)][num_to_remove] -= 1
-                if buckets[num_to_remove // (valueDiff + 1)][num_to_remove] == 0:
-                    del buckets[num_to_remove // (valueDiff + 1)][num_to_remove]
+                del buckets[num_to_remove // (valueDiff + 1)]
             i += 1
         return False
             
