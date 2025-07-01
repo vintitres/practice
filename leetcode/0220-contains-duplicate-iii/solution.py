9b21033e2c605aaa13de46c60f1eebe8f67862d8
vintitres <vintitres@users.noreply.github.com>
2025-07-01 02:10:11 +0000

    [LeetCode Sync] Runtime - 1017 ms (9.53%), Memory - 31.5 MB (67.86%)

---
+++ leetcode/0220-contains-duplicate-iii/solution.py
@@ -0,0 +1,17 @@
+class Solution:
+    def containsNearbyAlmostDuplicate(self, nums: List[int], indexDiff: int, valueDiff: int) -> bool:
+        window = SortedList()
+        for i, num in enumerate(nums):
+            # TODO find num in sorted ist woth bisect and see if neighbors close enough
+            if window:
+                j = window.bisect_right(num)
+                if min(abs(window[j] - num) if j < len(window) else valueDiff + 1, abs(window[j - 1] - num) if j - 1 < len(window) else valueDiff + 1) <= valueDiff:
+                    return True
+            window.add(num)
+            if i >= indexDiff:
+                window.remove(nums[i - indexDiff])
+            i += 1
+        print(window)
+        return False
+            
+        
